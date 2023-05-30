174adc1d5505df2280a346d2c9071c5ff8f113d5
chldbwls8974 <chldbwls8974@naver.com>
2023-05-30 15:06:56 +0900

    자바 : 변수 기본형 및 선언 방법 예제
    char, int, float, boolean

---
+++ java/src/day1/variable/FloatExample1.java
@@ -0,0 +1,15 @@
+package day1.variable;
+
+public class FloatExample1 {
+
+	public static void main(String[] args) {
+		double num1 = 3.14;
+		
+		// 기본 실수 리터럴은 타입이 double(8byte)이기 때문에 float 형태로 저장하기 위해서는 숫자 뒤에 f를 반드시 붙여야 함.
+		float num2 = 3.14f;
+		
+		
+		
+	}
+
+}
