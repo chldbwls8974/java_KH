f8b95349bceaf21bc2924c42a9b7ba4146855698
chldbwls8974 <chldbwls8974@naver.com>
2023-05-31 15:17:30 +0900

    과제1

--- java/src/day2/homework/ScanAssign1.java
+++ java/src/day2/homework/ScanAssign1.java
@@ -1,11 +1,19 @@
 package day2.homework;
 
+import java.util.*;
+
 public class ScanAssign1 {
 
-	// 
+	// 정수 두개와 산술 연사자를 입력받아 출력하시오.
+	// 입력: 1 + 2
+	// 출력: 1 + 2
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("두 정수와 산술연산자를 입력하세요.");
+		String str = sc.next();
+		
+		System.out.println(str);
 	}
 
 }
