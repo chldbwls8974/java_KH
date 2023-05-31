84f87a4b49989b9076f16daf7b32c50983466df8
chldbwls8974 <chldbwls8974@naver.com>
2023-05-31 11:06:45 +0900

    점수 합과 평균
    세가지 과목의 점수를 입력받고 합과 평균을 구한다.

--- java/src/day2/sample/Example.java
+++ java/src/day2/sample/Example.java
@@ -1,17 +1,28 @@
-
 package day2.sample;
 import java.util.Scanner;
+
 public class Example {
 
 	public static void main(String[] args) {
-		int Kor = 0;
-		int Eng = 0;
-		int Math = 0;
-		int sum = add(Kor, Eng, Math);
-		double avg = division(sum / 3);
+		int Kor;
+		int Eng;
+		int Math;
 		
-		System.out.println("세 과목의 합은" + sum + "입니다.");
-		System.out.println("세 과목의 평균은" + avg + "입니다.");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("국어 점수를 입력하세요.");
+		Kor = sc.nextInt();
+		System.out.println("영어 점수를 입력하세요.");
+		Eng = sc.nextInt();
+		System.out.println("수학 점수를 입력하세요.");
+		Math = sc.nextInt();
+		
+		int sum = add(Kor, Eng, Math);
+		double avg = division(sum);
+		
+		System.out.println("세 과목의 합은 " + sum + "입니다.");
+		System.out.println("세 과목의 평균은 " + avg + "입니다.");
 	}
 	
 	public static int add(int n1, int n2, int n3) {
@@ -20,7 +31,7 @@ public class Example {
 	}
 	
 	public static double division(int n) {
-		double result = n / 3;
+		double result = n / 3.0;
 		return result;
 	}
 	
