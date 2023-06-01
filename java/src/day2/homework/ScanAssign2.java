5877c957de96621781cf61f8b2cd14c8e1110520
chldbwls8974 <chldbwls8974@naver.com>
2023-06-01 09:38:32 +0900

    코드 간단화
    출력코드에서 산술연산자에 공백을 추가하여 문자열화 한다.

--- java/src/day2/homework/ScanAssign2.java
+++ java/src/day2/homework/ScanAssign2.java
@@ -12,11 +12,11 @@ public class ScanAssign2 {
 		System.out.println("두 정수를 입력하세요.");
 		int num1 = sc.nextInt(), num2 = sc.nextInt();
 
-		System.out.println(""+ num1 + "+" + num2 + "=" + (num1+num2));
-		System.out.println(""+ num1 + "-" + num2 + "=" + (num1-num2));
-		System.out.println(""+ num1 + "*" + num2 + "=" + (num1*num2));
-		System.out.println(""+ num1 + "/" + num2 + "=" + ((double)num1/num2));
-		System.out.println(""+ num1 + "mod" + num2 + "=" + (num1%num2));
+		System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
+		System.out.println(num1 + " - " + num2 + " = " + (num1-num2));
+		System.out.println(num1 + " * " + num2 + " = " + (num1*num2));
+		System.out.println(num1 + " / " + num2 + " = " + ((double)num1/num2));
+		System.out.println(num1 + " mod " + num2 + " = " + (num1%num2));
 		
 		sc.close();
 	}
