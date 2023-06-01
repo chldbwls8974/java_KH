09192107b69c874d5d7ef773c07e02fbd6c7f039
chldbwls8974 <chldbwls8974@naver.com>
2023-06-01 10:33:40 +0900

    교통수단

---
+++ book_example/chapter6/cooperation/Bus.java
@@ -0,0 +1,21 @@
+package cooperation;
+
+public class Bus {
+	//버스와 관련된 변수 설정. 
+	int busNumber;
+	int passengerCount;
+	int money;
+	
+	public Bus(int busNumber) {
+		this.busNumber = busNumber;
+	}
+	
+	public void take(int money) {
+		this.money += money;
+		passengerCount++;
+	}
+	
+	public void showInfo() {
+		System.out.println("버스" + busNumber + "번의 승객은 " + passengerCount + "명이고, 수입은 " + money + "입니다.");
+	}
+}
