a1bac5e0bae5c7df7e687369b1779911083bdcb6
chldbwls8974 <chldbwls8974@naver.com>
2023-06-01 14:47:29 +0900

    .

--- java/src/day3/practice/IfEx1.java
+++ java/src/day3/practice/IfEx1.java
@@ -7,7 +7,7 @@ public class IfEx1 {
 	public static void main(String[] args) {
 		// 정수 num을 입력받아서 num이 0인지 음수인지 양수인지 판별하는 코드
 		
-System.out.println("정수 하나를 입력하시오");
+		System.out.println("정수 하나를 입력하시오");
 		
 		Scanner sc = new Scanner(System.in);
 		
