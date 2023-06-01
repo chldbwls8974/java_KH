ea696ebe70cdfaf4298cee4b0b7d312dd6412747
chldbwls8974 <chldbwls8974@naver.com>
2023-06-01 14:58:02 +0900

    day 3 과제

---
+++ java/src/day3/homework/Assign1.java
@@ -0,0 +1,19 @@
+package day3.homework;
+
+import java.util.Scanner;
+
+public class Assign1 {
+
+	public static void main(String[] args) {
+		//성별을 입력받아 여성인지 확인하는 코드 (조건)
+		// input gender
+		// 입력: M
+		// Are you a women? men
+		// Are you a women? woman
+		
+		System.out.println("성별을 입력하시오. (남성인 경우 M, 여성인 경우 W)");
+		Scanner sc = new Scanner(System.in);
+		String gender = sc.next();
+	}
+
+}
