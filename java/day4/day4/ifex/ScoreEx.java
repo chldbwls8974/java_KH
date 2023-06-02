81d4b6f3080f096d1690c28fdcb9e506bfa56ea4
chldbwls8974 <chldbwls8974@naver.com>
2023-06-02 14:01:06 +0900

    학점 계산 4가지 버전

--- java/day4/day4/ifex/ScoreEx.java
+++ java/day4/day4/ifex/ScoreEx.java
@@ -15,12 +15,43 @@ public class ScoreEx {
 		Scanner sc = new Scanner(System.in);
 		int score = sc.nextInt();
 		
+		// version 1
+		/*
 		if (score >= 90 && score <= 100) {System.out.println("A");}
 		else if (score >= 80 && score <= 90) {System.out.println("B");}
 		else if (score >= 70 && score <= 80) {System.out.println("C");}
 		else if (score >= 60 && score <= 70) {System.out.println("D");}
-		else if (score < 60) {System.out.println("F");}
+		else if (score < 60 && score >= 0) {System.out.println("F");}
 		else {System.out.println("wrong score");}
+		*/
+		
+		// version 2
+		/*
+		if (score >= 0 && score < 60 ) {System.out.println("F");}
+		else if (score < 70) {System.out.println("D");}
+		else if (score < 80 ) {System.out.println("C");}
+		else if (score < 90) {System.out.println("B");}
+		else if (score <= 100) {System.out.println("A");}
+		else {System.out.println("wrong score");}
+		*/
+		
+		// version 3
+		/*
+		if (score <0 || score >100 ) {System.out.println("wrong score");}
+		else if (score < 60 ) {System.out.println("F");}
+		else if (score < 70) {System.out.println("D");}
+		else if (score < 80 ) {System.out.println("C");}
+		else if (score < 90) {System.out.println("B");}
+		else {System.out.println("A");}
+		*/
+		
+		// version 4
+		if (score <0 || score >100 ) {System.out.println("wrong score");}
+		else if (score >= 90 ) {System.out.println("A");}
+		else if (score >= 80) {System.out.println("B");}
+		else if (score >= 70 ) {System.out.println("C");}
+		else if (score >= 60) {System.out.println("D");}
+		else {System.out.println("F");}
 		
 		sc.close();
 	}
