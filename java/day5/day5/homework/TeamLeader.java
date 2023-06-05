14977a79cdc0c46f8939c75958f9960b1719f189
chldbwls8974 <chldbwls8974@naver.com>
2023-06-05 14:59:15 +0900

    5일차 과제

---
+++ java/day5/day5/homework/TeamLeader.java
@@ -0,0 +1,43 @@
+package day5.homework;
+import java.util.*;
+
+public class TeamLeader {
+
+	public static void main(String[] args) {
+
+		// 30명의 학생이 있는 반에서 1번부터 연속된 번호를 가진 3명의 학생이 1조가 되도로 나누었다.
+		// 이때 가장 큰 번호인 학생이 조장이 된다.
+		// 학생 번호가 주어졌을 때 조장인지 조원인지 판별하는 코드를 작성하세요.
+		// input student number
+		
+		
+		// 3의 배수는 팀장, 아닌 수는 팀원
+		
+		//switch 문
+		int studentMax = 30;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input student number");
+		int num = sc.nextInt();
+		
+		switch( studentMax % num ) {
+		case 0:
+			System.out.println("input student number " + num + "is a TeamLeader.");
+			break;
+			
+		case 1:
+			System.out.println("input student number " + num + "is a just Team Member.");
+			break;
+			
+		case 2:
+			System.out.println("input student number " + num + "is a just Team Member.");
+			break;
+			
+			default:
+				System.out.println("plz rewrite");
+		}
+		
+		
+		sc.close();
+	}
+
+}
---
+++ java/day5/day5/homework/UpDown.java
@@ -0,0 +1,18 @@
+package day5.homework;
+
+public class UpDown {
+
+	public static void main(String[] args) {
+
+		//UpDownEx
+		// 1에서 100사이의 랜덤 정수를 생성하고, 이 정수를 맞추는 게임을 구현하세요.
+		
+		// 만약에 랜덤한 수가 45라면
+		// input: 50
+		// 출력 Down!
+		// input : 40
+		// 출력 Up!
+		// 맞출때 까지 반복, 맞추면 반복문 탈출
+	}
+
+}
