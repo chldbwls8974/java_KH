2f5033ea752403dc93c1765b4defb9e77cd1f1a1
chldbwls8974 <chldbwls8974@naver.com>
2023-06-05 15:08:11 +0900

    5일차 과제 - 업다운

--- java/day5/day5/homework/UpDown.java
+++ java/day5/day5/homework/UpDown.java
@@ -1,5 +1,7 @@
 package day5.homework;
 
+import java.util.Scanner;
+
 public class UpDown {
 
 	public static void main(String[] args) {
@@ -13,6 +15,24 @@ public class UpDown {
 		// input : 40
 		// 출력 Up!
 		// 맞출때 까지 반복, 맞추면 반복문 탈출
+		
+		Scanner sc = new Scanner(System.in);
+		int min = 1, max = 100;
+		
+		int random =(int)( Math.random()*(max - min + 1) + min);
+		int num = 0;
+	
+		
+		while (random != num) {
+			System.out.println("input number");
+			num = sc.nextInt();
+			if(random > num ) {System.out.println("Up!");}
+			else if (random < num){System.out.println("Down!");}	
+		}
+		
+		System.out.println("Good!!" );
+		
+		sc.close();
 	}
 
 }
