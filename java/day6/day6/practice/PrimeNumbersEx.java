f3a7993ba1251d2c1e9501a249e3077cf14f160a
chldbwls8974 <chldbwls8974@naver.com>
2023-06-07 12:35:38 +0900

    2~100 소수 출력
    변수 위치 신경쓰기

---
+++ java/day6/day6/practice/PrimeNumbersEx.java
@@ -0,0 +1,20 @@
+package day6.practice;
+
+public class PrimeNumbersEx {
+
+	public static void main(String[] args) {
+
+		// 2부터 100사이의 모든 소수를 출력하는 코드
+		
+		
+		for(int i = 2 ; i <= 100 ; i++) {
+			int count = 0;
+			for(int j = 1 ; j <= i ; j++) {
+				if ( i % j == 0) count++;
+			}
+		
+			if( count == 2 ) {System.out.println(i);}
+		}
+	}
+
+}
