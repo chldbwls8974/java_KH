b26062b5745909b3880e327bd565081725fd135c
chldbwls8974 <chldbwls8974@naver.com>
2023-06-07 11:06:44 +0900

    성적 평균
    평균을 구하고, 계속하는지 아닌지 여부를 입력받아 반복문을 실행하고 빠져나온다.

---
+++ java/day6/day6/whileex/AverageEx.java
@@ -0,0 +1,34 @@
+package day6.whileex;
+
+import java.util.Scanner;
+
+public class AverageEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("국어, 영어, 수학 성적의 평균을 구하는 코드입니다.");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char ans = 'y';
+		
+		while(ans != 'n' && ans == 'y') {
+			System.out.println("국어, 영어, 수학 순서대로 성적을 입력하시오.");
+			
+			int kor = sc.nextInt();
+			int eng = sc.nextInt();
+			int math = sc.nextInt();
+			
+			System.out.println((kor + eng + math)/ 3.0);
+			System.out.println("continue? (y/n)");
+			ans = sc.next().charAt(0);
+		}
+		 System.out.println("EXIT");
+		 
+		 sc.close();
+		
+		
+	}
+
+}
