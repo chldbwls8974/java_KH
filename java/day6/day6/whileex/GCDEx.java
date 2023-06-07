af6a9c57928588f7a1b89e183e0b9fd93a8b539e
chldbwls8974 <chldbwls8974@naver.com>
2023-06-07 11:05:41 +0900

    최대공약수
    while의 조건에서 두 수의 대소를 비교할 필요가 없다 => 작은 수와 큰 수 사이의 수에서는 최대공약수가 나올 수 없기 때문

---
+++ java/day6/day6/whileex/GCDEx.java
@@ -0,0 +1,28 @@
+package day6.whileex;
+
+import java.util.Scanner;
+
+public class GCDEx {
+
+	public static void main(String[] args) {
+
+		// 두 수를 입력받아 최대공약수 출력
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("int number A and B");
+		int a = sc.nextInt(), b = sc.nextInt();
+		int i = 1, result = 0;
+		
+		while(a >= i) { // 큰 수와 작은 수 사이의 수들은 어차피 최대공약수가 될 수 없기때문에 조건에 크게 신경 쓸 필요 없음.
+			if(a % i == 0 && b % i == 0) {
+				result = i;
+			}
+			i++;
+		}
+		
+		System.out.println("answer is " + result);
+		sc.close();
+	}
+
+}
