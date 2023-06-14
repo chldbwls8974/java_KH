39e487596979312618d2afa2e63d48277cb8cfe2
chldbwls8974 <chldbwls8974@naver.com>
2023-06-14 14:54:17 +0900

    11일차 과제

---
+++ java/src/day11/homework/BaseballGameEx.java
@@ -0,0 +1,41 @@
+package day11.homework;
+
+import java.util.Scanner;
+
+public class BaseballGameEx {
+
+	public static void main(String[] args) {
+
+		// 숫자 야구 게임
+		// 1 ~ 9 사이의 중복되지 않은 세 수 를 맞추는 게임
+		// 규칙
+		// B: 같은 숫자가 있지만 위치가 다름
+		// S: 같은 숫자가 있고 위치가 같음
+		// 30 쓰리아웃 : 일치하는 숫자가 하나도 없음
+		// 정답 입력하면 3S
+		Scanner sc = new Scanner(System.in);
+		
+		int max = 9, min = 1;
+		int ran = (int)( Math.random()*(max - min + 1) +min);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+		
+	}
+
+}
