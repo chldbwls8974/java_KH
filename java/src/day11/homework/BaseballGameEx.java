3cb3dd7951e0b283d4cb1d982564f49e9fe287a0
chldbwls8974 <chldbwls8974@naver.com>
2023-06-14 21:19:31 +0900

    과제!

--- java/src/day11/homework/BaseballGameEx.java
+++ java/src/day11/homework/BaseballGameEx.java
@@ -6,36 +6,46 @@ public class BaseballGameEx {
 
 	public static void main(String[] args) {
 
-		// 숫자 야구 게임
-		// 1 ~ 9 사이의 중복되지 않은 세 수 를 맞추는 게임
-		// 규칙
-		// B: 같은 숫자가 있지만 위치가 다름
-		// S: 같은 숫자가 있고 위치가 같음
-		// 30 쓰리아웃 : 일치하는 숫자가 하나도 없음
-		// 정답 입력하면 3S
 		Scanner sc = new Scanner(System.in);
 		
-		int max = 9, min = 1;
 		int []base = new int[3];
 		int []ans = new int[3];
 		
-	
+		String result = "";
 		
-		System.out.println("input num");
+		randomArray(1, 9, base);
+		print(base);
+		
+		
+		do {
+		System.out.print("input num: ");
 		for(int i = 0 ; i < 3 ; i++) {
-			base[i] = (int)( Math.random()*(max - min + 1) + min);
 			ans[i] = sc.nextInt();
 		}
 		
-		print(base);
-		print(ans);
-		System.out.println(res(base,ans));		
+		if(getCount(base, ans)==9) { 
+			result = "3O";
+			System.out.println(result);
+		}
+		else if (getCount(base, ans)==0) {
+			result = "3S";
+			System.out.println(result);
+		}
+		else if (getCount(base, ans)!=9) {
+			res(base,ans);
+		}
+		} while(!result.equals("3S") );
+		
+		System.out.println("Good!");
 		
 		sc.close();
 		
 	}
 	
-	// 배열 출력 메서드
+	
+	// method 
+	
+	// print method
 	public static void print(int arr[]) {
 		for(int num : arr) {
 			System.out.print(num);
@@ -43,17 +53,62 @@ public class BaseballGameEx {
 		System.out.println();
 	}
 	
-	// 비교 메서드
-	public static String res(int arr1[], int arr2[]) {
+	// 3O check method
+	public static int getCount(int arr1[], int arr2[]) {
+		int count = 0;
 		for(int i = 0 ; i < arr1.length ; i++) {
 			for(int j = 0 ; j < arr1.length ; j++) {
-				if(arr1[i]==arr2[j]) {
-					if(i==j) return "S";
-					else return "B";
-				}
-				
+				if(arr1[i]!=arr2[j]) count++;	
 			}
 		}
-		return "";
+		return count;
+	}
+	
+	
+	// B and S check method
+	public static void res(int arr1[], int arr2[]) {
+		int s=0, b=0;
+		for(int i = 0 ; i < arr1.length ; i++) {
+			for(int j = 0 ; j < arr1.length ; j++) {
+				if(arr1[i] == arr2[j]) {
+					if(i==j)  s++;
+					else if (i!=j)  b++;
+				}
+			}
+		}
+		
+		if (s>0 && b>0) System.out.println(s + "S" + b +"B");
+		else if (s > 0) System.out.println(s + "S");
+		else System.out.println(b+"B");
+		
+	}
+
+	
+	// Methods to prevent duplication
+	public static boolean contains(int arr[], int num, int count) {
+		for(int i = 0 ; i < count ; i++) {
+			if(num == arr[i]) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+	
+	public static void randomArray(int min, int max, int arr[]) {
+		if(arr == null) return;
+
+		if(max - min + 1 < arr.length) return;
+		
+		
+		int count = 0; 
+		while(count < 3) {
+			
+			int ran = (int)( Math.random()*(max - min + 1) +min);
+			if(!contains(arr,ran,count)) {
+				arr[count] = ran;
+				count++;
+			}
+		}
 	}
 }
