4404c876e88b970d8e200ac7ca90050dac3076db
chldbwls8974 <chldbwls8974@naver.com>
2023-06-15 14:48:53 +0900

    Update Other.java

--- java/src/day11/homework/Other.java
+++ java/src/day11/homework/Other.java
@@ -2,117 +2,72 @@ package day11.homework;
 
 import java.util.Scanner;
 
+import array.Array;
+
 public class Other {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		
-		int []base = new int[3];
-		int []ans = new int[3];
+		int com[] = new int[3];
+		int user[] = new int[3];
+		int min = 1, max = 9;
+		int strike = 0, ball = 0;
 		
-		String result = "";
-		
-		randomArray(1, 9, base);
-		print(base);
+		Array.creatRandomArray2(min, max, com);
+		Array.printArray(com);
 		
 		do {
-		System.out.print("input num: ");
-		for(int i = 0 ; i < 3 ; i++) {
-			ans[i] = sc.nextInt();
-		}
-		if(getCount(base, ans)==9) { 
-			result = "3O";
-			System.out.println(result);
-		}
-		else {
-			System.out.println(res(base,ans)); 
-		}
-		} while(!res(base,ans).equals("3S") );
-		System.out.println("Good!");
+			System.out.println("user: ");
+			for(int i = 0 ; i < user.length ; i++) {
+				user[i] = sc.nextInt();
+			}
+			
+			// 사용자 입력 체크
+			
+			// 판별
+			// 스트라이크 개수 판별
+			
+			// 볼의 개수 판별
+			
+			// 스트라이크와 볼의 개수에 맞게 출력
+			
+		}while(strike < 3);
+		
+	
 		
 		sc.close();
 		
 	}
-	
-	
-	// method 
-	
-	// print method
-	public static void print(int arr[]) {
-		for(int num : arr) {
-			System.out.print(num);
+		
+	/** 스트라이크 개수를 판별하는 메서드
+	 * = > 두 배열에서 같은 번지에 있는 값들이 몇개 같은지 알려주는 메서드
+	 * 매개변수: 두 배열 int arr1[], int arr2[]	
+	 * 리턴타입: 몇개 같은지 int
+	 * 메서드명 : Strike
+	 */
+	/*
+	public static int strike(int arr1[], int arr2[]) {
+		// 배열 생성이 안된 배열이 하나라도 있으면 비교할 수 없어서 0을 리턴
+		if(arr1 == null || arr2 == null) {
+			return 0;
 		}
-		System.out.println();
-	}
-	
-	// 3O check method
-	public static int getCount(int arr1[], int arr2[]) {
+		
+		// 배열의 개수가 다른 경우 작은 크기의 배열을 기준으로 비교하기 위해 size 계산
+		int size = arr1.length < arr2.length ? arr1.length : arr2.length;
 		int count = 0;
-		for(int i = 0 ; i < arr1.length ; i++) {
-			for(int j = 0 ; j < arr1.length ; j++) {
-				if(arr1[i]!=arr2[j]) count++;	
+		for(int i = 0 ; i < size ; i++) {
+			if(arr1[i] == arr2[i]) {
+				
 			}
 		}
-		return count;
-	}
-	
-	
-	// B and S check method
-	public static String res(int arr1[], int arr2[]) {
-		int s=0, b=0;
-		String result="";
-		for(int i = 0 ; i < arr1.length ; i++) {
-			for(int j = 0 ; j < arr1.length ; j++) {
-				if(arr1[i] == arr2[j]) {
-					if(i==j)  s++;
-					else if (i!=j)  b++;
-				}
-			}
-		}
-		
-		if (s>0 && b>0) {
-			result = s + "S" + b + "B";
-			return result;
-		}
-		else if (s > 0) {
-			result = s + "S";
-			return result;
-		}
-		else { 
-			result = b +"B";
-			return result;
-			
-		}
-		
-	}
-
-	
-	// Methods to prevent duplication
-	public static boolean contains(int arr[], int num, int count) {
-		for(int i = 0 ; i < count ; i++) {
-			if(num == arr[i]) {
-				return true;
-			}
-		}
-		return false;
-
-	}
-	
-	public static void randomArray(int min, int max, int arr[]) {
-		if(arr == null) return;
-
-		if(max - min + 1 < arr.length) return;
-		
-		
-		int count = 0; 
-		while(count < 3) {
-			
-			int ran = (int)( Math.random()*(max - min + 1) +min);
-			if(!contains(arr,ran,count)) {
-				arr[count] = ran;
-				count++;
-			}
-		}
-	}
+	}*/
 }
+
+
+
+
+
+
+
