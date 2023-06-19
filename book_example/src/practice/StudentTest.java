a461a970204480419df2c280b94c78d3b61a5eb7
chldbwls8974 <chldbwls8974@naver.com>
2023-06-19 15:17:20 +0900

    자습

---
+++ book_example/src/practice/StudentTest.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		StudentManage std1 = new StudentManage();
+		std1.studentName="이지원";
+		
+		
+	}
+
+}
