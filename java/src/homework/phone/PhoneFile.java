e461e2d95199210cad71aca58bebaa8fb90682f7
chldbwls8974 <chldbwls8974@naver.com>
2023-06-30 15:22:31 +0900

    .

---
+++ java/src/homework/phone/PhoneFile.java
@@ -0,0 +1,10 @@
+package homework.phone;
+
+public class PhoneFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 전에 만들었던 전화번호부 프로그램을 파일에 저장하는 코드 생성
+	}
+
+}
