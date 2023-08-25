64f7624909ed8209c711c2b8995caf5e39051190
JOMINJEONGG <chldbwls8974@naver.com>
2023-08-25 17:19:01 +0900

    servlet day3
    부트스트랩을 이용.
    페이지네이션 생성

---
+++ servlet/project2/src/main/java/kr/kh/app/vo/BoardVO.java
@@ -0,0 +1,20 @@
+package kr.kh.app.vo;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BoardVO {
+	private int bo_num;
+	private String bo_title;
+	private String bo_me_id;
+	
+	// 게시글 리스트에서 사용할 url을 리턴해주는 메세드
+	//  /board/detail?bo_num=1
+	public String getBoardDetailUrl() {
+		return "/board/detail?bo_num="+bo_num;
+	}
+}
