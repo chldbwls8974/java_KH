8a058c4971def9746ef74eb03d40c1e812ab929b
JOMINJEONGG <chldbwls8974@naver.com>
2023-08-29 15:34:40 +0900

    spring day2
    회원가입 및 로그인 로그아웃 기능 추가

--- spring pojects/spring/src/main/java/kr/kh/spring/controller/MemberController.java
+++ spring pojects/spring/src/main/java/kr/kh/spring/controller/MemberController.java
@@ -1,23 +1,104 @@
 package kr.kh.spring.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.kh.spring.service.MemberService;
+import kr.kh.spring.util.Message;
+import kr.kh.spring.vo.MemberVO;
 
 @Controller
 public class MemberController {
 	
-	@Autowired
+	@Autowired // 객체 만들어주는 annotation, dynamic과 다르게 이렇게 @ 만 붙여줘도 됨.
 	private MemberService memberService;
 	
-	@RequestMapping(value="/signup", method=RequestMethod.GET)
+	// url 과 연결해주는 @
+	// dynamic과 다르게 get post를 하나의 메서드로 통일하여 사용이 가능
+	@RequestMapping(value="/member/signup", method=RequestMethod.GET)
 	public String signup() {
-		int count = memberService.count();
-		System.out.println(count);
 		return "member/signup";
 	}
+	
+	@RequestMapping(value="/member/signup", method=RequestMethod.POST)
+	public String signupPost(MemberVO member, Model model) {
+		Message msg = new Message("/member/signup","회원가입 실패");
+		if(memberService.signup(member)) {
+			msg = new Message("/member/signup","회원가입 성공");
+		}
+		
+		model.addAttribute("msg",msg);
+		return "message";
+	}
+	
+	
+	
+	@GetMapping(value="/member/login")
+	public String memberlogin() {
+		return "member/login";
+	}
+	
+	// model은 화면에 전송해줄때 사용
+	@PostMapping(value="/member/login")
+	public String memberLoginPost(MemberVO member, Model model) {
+		Message msg = new Message("/member/login","로그인 실패");
+		MemberVO user = memberService.login(member);
+		
+		if(user!=null) {
+			msg = new Message("/","로그인 성공");
+		}
+		model.addAttribute("user",user);
+		model.addAttribute("msg",msg);
+		return "message";
+	}
+	
+	
+	@GetMapping(value="/member/logout")
+	public String memberLogout(HttpServletRequest request, Model model) {
+		HttpSession session = request.getSession();
+		MemberVO user =(MemberVO)session.getAttribute("user");
+		Message msg = new Message("/",null);
+		
+		if(user != null) {
+			session.removeAttribute("user");
+			msg.setMsg("로그아웃 성공");
+		}
+		model.addAttribute("msg",msg);
+		
+		return "message";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- spring pojects/spring/src/main/webapp/WEB-INF/views/member/login.jsp
+++ spring pojects/spring/src/main/webapp/WEB-INF/views/member/login.jsp
@@ -1,18 +1,18 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page language="java" contentType="text/html; charset=utf-8" 
-	pageEncoding="utf-8" %>
-<!doctype html>
-<html lang="ko">
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 	<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-	<title>스프링</title>
+<title>로그인</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <!-- Brand/logo -->
   <a class="navbar-brand" href="<c:url value='/' />">
     <img src="<c:url value='/resources/img/logo.jpg'/>" alt="logo" style="width:40px;">
@@ -20,7 +20,7 @@
   
   <!-- Links -->
   <ul class="navbar-nav">
-	<c:if test="${user ==null}">
+   <c:if test="${user ==null}">
 	    <li class="nav-item">
 	      <a class="nav-link" href="<c:url value='/member/signup'/>">회원가입</a>
 	    </li>
@@ -35,8 +35,44 @@
     </c:if>
   </ul>
 </nav>
-
-
-
+<div class="container">
+	<h1>로그인</h1>
+ 	<form action="<c:url value='/member/login'/>" method="post">
+ 		<div class = "form-group">
+ 			<label>아이디</label>
+ 			<input type="text" class="form-control" name="me_id">
+ 		</div>
+ 		<div class = "form-group">
+ 			<label>비번</label>
+ 			<input type="password" class="form-control" name="me_pw">
+ 		</div>
+ 		<button class="btn btn-outline-warning col-12">로그인</button>
+ 	</form>	
+</div>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
